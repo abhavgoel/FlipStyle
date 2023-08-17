8b5189330563cac2a8080a56c84435672be90011
Abhav <goelabhav2002@gmail.com>
2023-08-17 21:14:49 +0530

    chatbot working on home pasge

--- streamlit app/homepage.py
+++ streamlit app/homepage.py
@@ -1,17 +1,21 @@
 import streamlit as st
+from openaiApi import *
+
+
 st.set_page_config(
-    page_title="Streamlit Homepage",
+    page_title="FlipStyle",
     page_icon="🧊",
 )
-st.title("Streamlit Homepage")
-st.sidebar.success("Select a page above")
+st.title("FlipStyle")
+# st.sidebar.success("Select a page above")
 
 if("my_input" not in st.session_state):
     st.session_state["my_input"] = ""
 
-my_input = st.text_input("Input a a text here", st.session_state["my_input"])
+my_input = st.text_input("Describe an outfit you want to generate", st.session_state["my_input"])
 submit = st.button("Submit")
 if(submit):
     st.session_state["my_input"] = my_input
-    st.write("You submitted: ", my_input)
-    #st.experimental_rerun()
+    reply = outfit_generator(my_input)
+    st.write(reply)
+    
