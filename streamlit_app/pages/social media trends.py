f9fc4692e288c0d77a1ec03670d641854a1921be
Abhav <goelabhav2002@gmail.com>
2023-08-19 19:04:53 +0530

    used pathlib

--- streamlit_app/pages/social media trends.py
+++ streamlit_app/pages/social media trends.py
@@ -1,40 +1,31 @@
 import streamlit as st
-import os
+from pathlib import Path
 from PIL import Image
 from pinterest_scraper import *
 
 def get_images_from_directory(directory):
     images = []
-    for filename in os.listdir(directory):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-            images.append(os.path.join(directory, filename))
+    for file in directory.iterdir():
+        if file.suffix.lower() in ('.png', '.jpg', '.jpeg'):
+            images.append(file)
     return images
-
 
 st.title("Latest fashion trends from pinterest")
 show_trends = st.checkbox("Show latest fashion trends from Pinterest")
 
 if show_trends:
-        st.write("Fetching and displaying fashion trends...")
-        output = pinterest_images("fashion")
-        fashion_images=[]
-        if output:
-            # directory_path = os.path.abspath("pins")
-            current_directory = os.path.dirname(os.path.abspath(__file__))
-
-            # Navigate one directory back
-            parent_directory = os.path.dirname(current_directory)
-
-            # Construct the path to "pins" directory in the parent directory
-            pins_directory = os.path.join(parent_directory, "pins")
-            fashion_images = get_images_from_directory(pins_directory)
+    st.write("Fetching and displaying fashion trends...")
+    output = pinterest_images("fashion")
+    fashion_images = []
+    if output:
+        current_directory = Path(__file__).resolve().parent
+        pins_directory = current_directory.parent / "pins"
+        fashion_images = get_images_from_directory(pins_directory)
         
-        if fashion_images:
-            st.write(f"Found {len(fashion_images)} fashion trend images:")
-            for image_path in fashion_images:
-                image = Image.open(image_path)
-                st.image(image, caption=os.path.basename(image_path), use_column_width=True)
-        else:
-            st.write("No fashion trend images found.")
-
-
+    if fashion_images:
+        st.write(f"Found {len(fashion_images)} fashion trend images:")
+        for image_path in fashion_images:
+            image = Image.open(image_path)
+            st.image(image, caption=image_path.name, use_column_width=True)
+    else:
+        st.write("No fashion trend images found.")
